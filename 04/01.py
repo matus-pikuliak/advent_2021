4fcc6f638d019847a20a778ca06c07c362d3af37
Matúš Pikuliak <matus.pikuliak@gmail.com>
2021-12-04 11:23:24 +0100

    Day 04

--- 04/01.py
+++ 04/01.py
@@ -3,14 +3,17 @@ from itertools import chain
 lines = open('input').read().splitlines()
 
 o = {int(n): i for i, n in enumerate(lines[0].split(','))}
-boards_ver = [
+boards = [
     [list(map(int, l.split())) for l in b]
     for b in zip(*[lines[i::6] for i in range(2, 7)])
 ]
-boards_hor = [list(zip(*b)) for b in boards_ver]
 
 
-def calculate(board):
+def score(board):
+    return min(score_rows(board), score_rows(list(zip(*board))))
+
+
+def score_rows(board):
     steps = min(max(o[num] for num in row) for row in board)
     score = sum(num for num in chain(*board) if o[num] > steps)
     score *= next(k for k, v in o.items() if v == steps)
@@ -18,11 +21,7 @@ def calculate(board):
 
 
 # Part 1
-print(min(map(calculate, boards_ver + boards_hor)))
+print(min(map(score, boards)))
 
 # Part 2
-print(max(
-    min(calculate(b1), calculate(b2))
-    for b1, b2
-    in zip(boards_ver, boards_hor)
-))
+print(max(map(score, boards)))
