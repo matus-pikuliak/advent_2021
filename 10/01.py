f91ff5248c667504cac766c6a31aa266d3316a7f
Matúš Pikuliak <matus.pikuliak@gmail.com>
2021-12-10 12:27:12 +0100

    Day 10

--- 10/01.py
+++ 10/01.py
@@ -9,7 +9,7 @@ for line in open('input').read().splitlines():
     while len(line) > len(line := line.replace('<>', '').replace('[]', '').replace('{}', '').replace('()', '')): pass
     try:
         bracket = line[min(line.index(w) for w in wrong if w in line) + 1]
-        p1 += dict(zip(r, [3, 57, 1197, 25137]))[bracket]
+        p1 += [3, 57, 1197, 25137][r.index(bracket)]
     except ValueError:
         p2.add(sum((l.index(c) + 1) * 5 ** i for i, c in enumerate(line)))
 
