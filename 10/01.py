218f66e6e11dd09e4e9c88319d8eba37ff2471bc
Matúš Pikuliak <matus.pikuliak@gmail.com>
2021-12-10 12:01:32 +0100

    Day 10

---
+++ 10/01.py
@@ -0,0 +1,16 @@
+from itertools import product
+from statistics import median
+
+l, r = '([{<', ')]}>'
+wrong = set(map(''.join, set(product(l, r)) - set(zip(l, r))))
+
+p1, p2 = 0, set()
+for line in open('input').read().splitlines():
+    while len(line) > len(line := line.replace('<>', '').replace('[]', '').replace('{}', '').replace('()', '')): pass
+    try:
+        bracket = line[min(line.index(w) for w in wrong if w in line) + 1]
+        p1 += {c: i for c, i in zip(r, [3, 57, 1197, 25137])}[bracket]
+    except ValueError:
+        p2.add(sum((l.index(c) + 1) * 5 ** i for i, c in enumerate(line)))
+
+print(p1, median(p2))
