57d0c86dfc7cf79e984bba8bc1bdf0976e81684f
Matúš Pikuliak <matus.pikuliak@gmail.com>
2021-12-12 22:43:32 +0100

    Day 12 fast variant

--- 12/01b.py
+++ 12/01b.py
@@ -10,19 +10,19 @@ for line in open('input'):
 small = {node for node in n if node.islower() and node not in {'start', 'end'}}
 
 
-def make_num(last, smalls, double, part2):
+def make_num(last, visited, double, part2):
     if last == 'start':
         return 0
     if last == 'end':
         return 1
     if last.islower():
-        if last not in smalls:
-            return num(last, smalls | {last}, double, part2)
+        if last not in visited:
+            return num(last, visited | {last}, double, part2)
         elif part2 and not double:
             return num(last, small, small, part2)
         else:
             return 0
-    return num(last, smalls, double, part2)
+    return num(last, visited, double, part2)
 
 
 def num(last, smalls, double, part2=False):
