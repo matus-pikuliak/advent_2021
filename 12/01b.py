d29c47681d5d9cd776534152c031f038bfcf8b89
Matúš Pikuliak <matus.pikuliak@gmail.com>
2021-12-12 22:42:38 +0100

    Day 12 fast variant

---
+++ 12/01b.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+n = defaultdict(lambda: set())
+
+for line in open('input'):
+    x, y = line.strip().split('-')
+    n[x].add(y)
+    n[y].add(x)
+
+small = {node for node in n if node.islower() and node not in {'start', 'end'}}
+
+
+def make_num(last, smalls, double, part2):
+    if last == 'start':
+        return 0
+    if last == 'end':
+        return 1
+    if last.islower():
+        if last not in smalls:
+            return num(last, smalls | {last}, double, part2)
+        elif part2 and not double:
+            return num(last, small, small, part2)
+        else:
+            return 0
+    return num(last, smalls, double, part2)
+
+
+def num(last, smalls, double, part2=False):
+    return sum(
+        make_num(node, smalls, double, part2)
+        for node in n[last]
+    )
+
+
+print(num('start', set(), None, False))
+print(num('start', set(), None, True))
