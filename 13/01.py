4eff23b0fc00b1d866a06f176d9cd8c82a3521c2
Matúš Pikuliak <matus.pikuliak@gmail.com>
2021-12-13 12:02:59 +0100

    Day 13

--- 13/01.py
+++ 13/01.py
@@ -1,25 +1,21 @@
 points, folds = open('input').read().split('\n\n')
-points = set(
-    tuple(line.split(','))
+points = list(
+    list(map(int, line.split(',')))
     for line in points.split()
 )
 
 for i, fold in enumerate(folds.split('\n')):
-    dim = fold.split('=')[0][-1]
-    num = int(fold.split('=')[1])
-    for p in set(points):
-        val, imag = {
-            'y': (p.imag, 1j),
-            'x': (p.real, 1)
-        }[dim]
-        if val > num:
-            points.add(p - imag * 2 * (val - num))
-            points.remove(p)
+    dim, num = fold.split('=')
+    dim = dim[-1] == 'y'
+    num = int(num)
+    for p in points:
+        p[dim] -= 2 * (p[dim] - num) * (p[dim] > num)
     if not i:
-        print(len(points))
+        print(len(set(map(tuple, points))))
 
+points = set(map(tuple, points))
 for i in range(10):
     for j in range(100):
-        print('#' if j + 1j * i in points else ' ', end='')
+        print('#' if (j, i) in points else ' ', end='')
     print()
 
